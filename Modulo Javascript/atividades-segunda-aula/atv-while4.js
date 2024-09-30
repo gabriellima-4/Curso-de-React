6494fd166c6adb23c8a88b1342bcf84af3705fc5
Gabriel Lima <gabrilima2004@gmail.com>
2024-09-30 15:49:20 -0300

    esqueci dessa atividade

--- Modulo Javascript/atividades-segunda-aula/atv-while4.js
+++ Modulo Javascript/atividades-segunda-aula/atv-while4.js
@@ -1,3 +1,16 @@
 //verificar se é primo: crie um programa que verifique se um número é primo e printe true ou false.
 // to tomando uma surra nesse pfv me ajuda ai
 
+function numeroPrimo(numero){
+    if(numero < 2){
+        return false;
+    }
+    
+    for (let i = 2; i < Math.sqrt(numero); i++) {
+        if (numero % i === 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
